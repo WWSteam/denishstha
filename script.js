273b1fad9fbdda71be90ae2c2aeab54e1a25bfb1
Denish Shrestha <neta.stha10@gmail.com>
2026-02-18 20:52:31 +0545

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("nav-menu");
+
+    toggle.addEventListener("click", function () {
+      menu.classList.toggle("show")
+    });
+  });
+</script>
