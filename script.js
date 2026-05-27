ac631d4dd2a2331bc08647bbc1a6470b98bc2c39
DenishStha <neta.stha10@gmail.com>
2026-05-27 16:00:29 +0545

    Modified

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   const toggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("nav-menu");
 
   toggle.addEventListener("click", function () {
     menu.classList.toggle("show");
   });
+
 });
